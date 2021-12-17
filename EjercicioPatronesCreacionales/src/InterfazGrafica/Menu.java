2bcdbcf192deeaf993c2ba2835558aefb21f1d18
unknown <manaranjoj@correo.udistrital.edu.co>
2021-12-17 12:03:16 -0500

    Se corrige una validacion

--- EjercicioPatronesCreacionales/src/InterfazGrafica/Menu.java
+++ EjercicioPatronesCreacionales/src/InterfazGrafica/Menu.java
@@ -12,140 +12,144 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 public class Menu extends JFrame {
 
-	private int opcion = 0;
-	private int Qpers = 0;
-	
-	private JPanel contentPane;
-	private JLabel lblAvisoSPer;
-	private JLabel lblAviso1;
-	private JButton btnElf;
-	private JButton btnEnano;
-	private JButton btnHumano;
-	private JLabel lblAvisoG;
-	private JTextField tFNumPers;
-	private JButton btnGenerar;
+    private int opcion = 0;
+    private int Qpers = 0;
 
-	/**
-	 * Crea el Frame.
-	 */
-	public Menu() {
+    private JPanel contentPane;
+    private JLabel lblAvisoSPer;
+    private JLabel lblAviso1;
+    private JButton btnElf;
+    private JButton btnEnano;
+    private JButton btnHumano;
+    private JLabel lblAvisoG;
+    private JTextField tFNumPers;
+    private JButton btnGenerar;
 
-		setBackground(Color.DARK_GRAY);
-		setTitle("Personajes");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(400, 400, 500, 400);
-		contentPane = new JPanel();
-		contentPane.setBackground(Color.LIGHT_GRAY);
-		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		setLocationRelativeTo(null);
+    /**
+     * Crea el Frame.
+     */
+    public Menu() {
 
-		lblAvisoSPer = new JLabel("Selector de Personajes");
-		lblAvisoSPer.setFont(new Font("Gill Sans MT", Font.BOLD, 25));
-		lblAvisoSPer.setBounds(108, 55, 269, 63);
-		contentPane.add(lblAvisoSPer);
+        setBackground(Color.DARK_GRAY);
+        setTitle("Personajes");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(400, 400, 500, 400);
+        contentPane = new JPanel();
+        contentPane.setBackground(Color.LIGHT_GRAY);
+        contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+        setLocationRelativeTo(null);
 
-		lblAviso1 = new JLabel("Por favor, seleccione al personaje que desea generar:");
-		lblAviso1.setFont(new Font("Gill Sans MT", Font.PLAIN, 15));
-		lblAviso1.setBounds(74, 106, 327, 63);
-		contentPane.add(lblAviso1);
+        lblAvisoSPer = new JLabel("Selector de Personajes");
+        lblAvisoSPer.setFont(new Font("Gill Sans MT", Font.BOLD, 25));
+        lblAvisoSPer.setBounds(108, 55, 269, 63);
+        contentPane.add(lblAvisoSPer);
 
-		btnElf = new JButton("Elfo");
-		btnElf.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				opcion = 1;
-				lblAvisoG.setEnabled(true);
-				tFNumPers.setEnabled(true);
-			}
-		});
-		btnElf.setBounds(84, 174, 89, 23);
-		contentPane.add(btnElf);
+        lblAviso1 = new JLabel("Por favor, seleccione al personaje que desea generar:");
+        lblAviso1.setFont(new Font("Gill Sans MT", Font.PLAIN, 15));
+        lblAviso1.setBounds(74, 106, 327, 63);
+        contentPane.add(lblAviso1);
 
-		btnEnano = new JButton("Enano");
-		btnEnano.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				opcion = 2;
-				lblAvisoG.setEnabled(true);
-				tFNumPers.setEnabled(true);
-			}
-		});
-		btnEnano.setBounds(183, 174, 89, 23);
-		contentPane.add(btnEnano);
+        btnElf = new JButton("Elfo");
+        btnElf.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                opcion = 1;
+                lblAvisoG.setEnabled(true);
+                tFNumPers.setEnabled(true);
+            }
+        });
+        btnElf.setBounds(84, 174, 89, 23);
+        contentPane.add(btnElf);
 
-		btnHumano = new JButton("Humano");
-		btnHumano.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				opcion = 3;
-				lblAvisoG.setEnabled(true);
-				tFNumPers.setEnabled(true);
-			}
-		});
-		btnHumano.setBounds(282, 174, 89, 23);
-		contentPane.add(btnHumano);
+        btnEnano = new JButton("Enano");
+        btnEnano.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                opcion = 2;
+                lblAvisoG.setEnabled(true);
+                tFNumPers.setEnabled(true);
+            }
+        });
+        btnEnano.setBounds(183, 174, 89, 23);
+        contentPane.add(btnEnano);
 
-		lblAvisoG = new JLabel("Cuantos desea Generar:");
-		lblAvisoG.addComponentListener(new ComponentAdapter() {
-		});
-		lblAvisoG.setEnabled(false);
-		lblAvisoG.setFont(new Font("Gill Sans MT", Font.PLAIN, 12));
-		lblAvisoG.setBounds(74, 217, 128, 23);
-		contentPane.add(lblAvisoG);
+        btnHumano = new JButton("Humano");
+        btnHumano.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                opcion = 3;
+                lblAvisoG.setEnabled(true);
+                tFNumPers.setEnabled(true);
+            }
+        });
+        btnHumano.setBounds(282, 174, 89, 23);
+        contentPane.add(btnHumano);
 
-		tFNumPers = new JTextField();
-		tFNumPers.setEnabled(false);
-		tFNumPers.setColumns(10);
-		tFNumPers.setBounds(206, 221, 86, 20);
-		contentPane.add(tFNumPers);
+        lblAvisoG = new JLabel("Cuantos desea Generar:");
+        lblAvisoG.addComponentListener(new ComponentAdapter() {
+        });
+        lblAvisoG.setEnabled(false);
+        lblAvisoG.setFont(new Font("Gill Sans MT", Font.PLAIN, 12));
+        lblAvisoG.setBounds(74, 217, 128, 23);
+        contentPane.add(lblAvisoG);
 
-		tFNumPers.addKeyListener(new KeyAdapter() {
-			public void keyTyped(KeyEvent e) {
+        tFNumPers = new JTextField();
+        tFNumPers.setEnabled(false);
+        tFNumPers.setColumns(10);
+        tFNumPers.setBounds(206, 221, 86, 20);
+        contentPane.add(tFNumPers);
 
-				if (tFNumPers.getText().length() == 1) {
-					e.consume();
-				}
-				
-				btnGenerar.setEnabled(true);
-			}
-		});
-		
-		
-		btnGenerar = new JButton("Generar");
-		btnGenerar.setFont(new Font("Gill Sans MT", Font.BOLD, 12));
-		btnGenerar.setEnabled(false);
-		btnGenerar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+        tFNumPers.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
 
-				Qpers = Integer.parseInt(tFNumPers.getText());
-				ventana vent = new ventana(opcion, Qpers);
-				vent.setVisible(true);
-				dispose();
-			}
+                if (tFNumPers.getText().length() == 1) {
+                    e.consume();
+                }
 
-		});
-		btnGenerar.setBounds(183, 306, 89, 23);
-		contentPane.add(btnGenerar);
+                btnGenerar.setEnabled(true);
+            }
+        });
 
-		Image img = new ImageIcon("Imagenes\\Fondo.jpg").getImage();
-		ImageIcon img2 = new ImageIcon(img.getScaledInstance(484, 361, Image.SCALE_SMOOTH));
-		JLabel lblFondo = new JLabel("");
-		lblFondo.setBackground(Color.BLACK);
-		lblFondo.setIcon(img2);
-		lblFondo.setBounds(0, 0, 484, 361);
-		contentPane.add(lblFondo);
-	}
+        btnGenerar = new JButton("Generar");
+        btnGenerar.setFont(new Font("Gill Sans MT", Font.BOLD, 12));
+        btnGenerar.setEnabled(false);
+        btnGenerar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (Integer.parseInt(tFNumPers.getText()) == 0) {
+                    JOptionPane.showMessageDialog(null, "Ingrese una cantidad mayor a 0");
+                }else{
 
-	public int getOpcion() {
-		return opcion;
-	}
+                Qpers = Integer.parseInt(tFNumPers.getText());
+                ventana vent = new ventana(opcion, Qpers);
+                vent.setVisible(true);
+                dispose();
+                }
+            }
 
-	public void setOpcion(int opcion) {
-		this.opcion = opcion;
-	}
+        });
+        btnGenerar.setBounds(183, 306, 89, 23);
+        contentPane.add(btnGenerar);
+
+        Image img = new ImageIcon("Imagenes\\Fondo.jpg").getImage();
+        ImageIcon img2 = new ImageIcon(img.getScaledInstance(484, 361, Image.SCALE_SMOOTH));
+        JLabel lblFondo = new JLabel("");
+        lblFondo.setBackground(Color.BLACK);
+        lblFondo.setIcon(img2);
+        lblFondo.setBounds(0, 0, 484, 361);
+        contentPane.add(lblFondo);
+    }
+
+    public int getOpcion() {
+        return opcion;
+    }
+
+    public void setOpcion(int opcion) {
+        this.opcion = opcion;
+    }
 }
