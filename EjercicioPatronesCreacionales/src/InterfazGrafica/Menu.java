721d99a11511b439139b301782d42d7d35f6f6c5
cristian <65404175+christian7075@users.noreply.github.com>
2021-12-17 09:59:03 -0500

    interfaz modificada

--- EjercicioPatronesCreacionales/src/InterfazGrafica/Menu.java
+++ EjercicioPatronesCreacionales/src/InterfazGrafica/Menu.java
@@ -26,7 +26,7 @@ import java.awt.event.KeyEvent;
 
 public class Menu extends JFrame {
 
-	private int opcion = 0;
+	private int opcion;
 	private JPanel contentPane;
 	private JLabel lblAvisoSPer;
 	private JLabel lblAviso1;
@@ -36,13 +36,12 @@ public class Menu extends JFrame {
 	private JLabel lblAvisoG;
 	private JTextField tFNumPers;
 	private JButton btnGenerar;
-	
 
 	/**
 	 * Crea el Frame.
 	 */
 	public Menu() {
-		
+
 		setBackground(Color.DARK_GRAY);
 		setTitle("Personajes");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -53,17 +52,17 @@ public class Menu extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		setLocationRelativeTo(null);
-		
+
 		lblAvisoSPer = new JLabel("Selector de Personajes");
 		lblAvisoSPer.setFont(new Font("Gill Sans MT", Font.BOLD, 25));
 		lblAvisoSPer.setBounds(108, 55, 269, 63);
 		contentPane.add(lblAvisoSPer);
-		
+
 		lblAviso1 = new JLabel("Por favor, seleccione al personaje que desea generar:");
 		lblAviso1.setFont(new Font("Gill Sans MT", Font.PLAIN, 15));
 		lblAviso1.setBounds(74, 106, 327, 63);
 		contentPane.add(lblAviso1);
-		
+
 		btnElf = new JButton("Elfo");
 		btnElf.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -75,7 +74,6 @@ public class Menu extends JFrame {
 		btnElf.setBounds(84, 174, 89, 23);
 		contentPane.add(btnElf);
 
-		
 		btnEnano = new JButton("Enano");
 		btnEnano.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -87,7 +85,6 @@ public class Menu extends JFrame {
 		btnEnano.setBounds(183, 174, 89, 23);
 		contentPane.add(btnEnano);
 
-		
 		btnHumano = new JButton("Humano");
 		btnHumano.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -98,9 +95,7 @@ public class Menu extends JFrame {
 		});
 		btnHumano.setBounds(282, 174, 89, 23);
 		contentPane.add(btnHumano);
-		
-		
-		
+
 		lblAvisoG = new JLabel("Cuantos desea Generar:");
 		lblAvisoG.addComponentListener(new ComponentAdapter() {
 		});
@@ -108,56 +103,40 @@ public class Menu extends JFrame {
 		lblAvisoG.setFont(new Font("Gill Sans MT", Font.PLAIN, 12));
 		lblAvisoG.setBounds(74, 217, 128, 23);
 		contentPane.add(lblAvisoG);
-		
+
 		tFNumPers = new JTextField();
 		tFNumPers.setEnabled(false);
 		tFNumPers.setColumns(10);
 		tFNumPers.setBounds(206, 221, 86, 20);
 		contentPane.add(tFNumPers);
-		
+
 		tFNumPers.addKeyListener(new KeyAdapter() {
 			public void keyTyped(KeyEvent e) {
-				
+
 				if (tFNumPers.getText().length() == 1) {
 					e.consume();
 				}
 				btnGenerar.setEnabled(true);
 			}
 		});
-		
+
 		btnGenerar = new JButton("Generar");
 		btnGenerar.setFont(new Font("Gill Sans MT", Font.BOLD, 12));
 		btnGenerar.setEnabled(false);
 		btnGenerar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				switch (opcion) {
-	            case 1:
-	            	VentanaElfo elf = new VentanaElfo();
-	            	elf.setVisible(true);
-	            	dispose();
-	            	
-	                break;
-	            case 2:
-	            	VentanaEnano ena = new VentanaEnano();
-	            	ena.setVisible(true);
-	            	dispose();
-	                break;
-	            case 3:
-	                VentanaHumano hum = new VentanaHumano();
-	                hum.setVisible(true);
-	                dispose();
-	                break;
-	        }}
-			
+
+				ventana vent = new ventana(opcion);
+				vent.setVisible(true);
+				dispose();
+			}
+
 		});
 		btnGenerar.setBounds(183, 306, 89, 23);
 		contentPane.add(btnGenerar);
-		
-		
-		
-		Image img= new ImageIcon("Imagenes\\Fondo.jpg").getImage();
-		ImageIcon img2=new ImageIcon(img.getScaledInstance(484, 361, Image.SCALE_SMOOTH));
+
+		Image img = new ImageIcon("Imagenes\\Fondo.jpg").getImage();
+		ImageIcon img2 = new ImageIcon(img.getScaledInstance(484, 361, Image.SCALE_SMOOTH));
 		JLabel lblFondo = new JLabel("");
 		lblFondo.setBackground(Color.BLACK);
 		lblFondo.setIcon(img2);
